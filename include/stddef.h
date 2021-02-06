52207d4276bf79dc6540590c502616a1533b5e40
Ingo Albrecht <commit@promovicz.org>
2021-02-06 10:48:27 +0100

    include: New stddef.h

---
+++ include/stddef.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Ingo Albrecht
+ * All rights reserved.  The Berkeley software License Agreement
+ * specifies the terms and conditions for redistribution.
+ *
+ */
+
+#include <sys/types.h>
+
+#ifndef NULL
+#define NULL 0
+#endif
